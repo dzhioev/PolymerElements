fa5343b3d6cf1d47be1ec08bafebbae24f0ca8c7
Pavel Sergeev <dzhioev@gmail.com>
2015-06-25 19:49:10 -0500

    Updated to the latest heads. init.py improved.

--- init.py
+++ init.py
@@ -10,8 +10,16 @@ REPOS = [
   'PolymerLabs/promise-polyfill',
   'PrismJS/prism',
   'web-animations/web-animations-js',
-  'chjj/marked'
+  'chjj/marked',
+  'Polymer/web-component-tester',
+  'mochajs/mocha'
 ];
+
+def GetExistingSubmodules():
+  return [l.split()[1] for l in str(git.submodule()).split('\n')
+                          if len(l.split()) > 1]
+
+git.submodule.update('--recursive', '--init')
 
 repos = [dict(name=full_name.split('/')[1],
               full_name=full_name,
@@ -21,13 +29,33 @@ repos = [dict(name=full_name.split('/')[1],
 repos += json.loads(
     str(curl('https://api.github.com/orgs/PolymerElements/repos?per_page=999')))
 
-existing_submodules = str(cut(git.submodule(), f='3', d=' ')).strip().split()
+existing_submodules = GetExistingSubmodules()
+repos_submodules = [repo['name'] for repo in repos]
+
+submodules_to_add = \
+    [s for s in repos_submodules if s not in existing_submodules]
+submodules_to_remove = \
+    [s for s in existing_submodules if s not in repos_submodules]
+
+print "To add: ", submodules_to_add
+print "To remove: ", submodules_to_remove
+
+for s in submodules_to_remove:
+  print 'Removing ' + s + '...'
+  git.submodule.deinit('--force', s)
+  git.rm(s)
 
 for repo in repos:
-  if repo['name'] in existing_submodules:
-    print 'Skipped ' + repo['name']
+  if repo['name'] not in submodules_to_add:
     continue
-
   print 'Adding ' + repo['full_name'] + '...'
-  git.submodule.add(repo['ssh_url'], repo['name'])
+  git.submodule.add('--force', repo['ssh_url'], repo['name'])
   git.submodule.update('--recursive', '--init')
+
+existing_submodules = GetExistingSubmodules()
+print 'Fetching remotes...'
+git.submodule.foreach('git', 'fetch')
+git.submodule.foreach('git', 'fetch', '--tags')
+print 'Updating...'
+git.submodule.foreach('git', 'checkout', 'origin/HEAD')
+git.submodule.foreach('git', 'submodule', 'update', '--init', '--recursive')
